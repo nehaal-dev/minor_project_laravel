4f882c4047adc0fc8e332bb80ecbc6d8d925cf74
Nehal Khan <nehalkhan4639@gmail.com>
2026-08-02 12:33:56 +0530

    Implement customer API and API resource

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CustomerResource;
 use Illuminate\Http\Request;
 use App\Models\Customer;
 
@@ -12,22 +13,42 @@ class CustomerController extends Controller
 
     public function index(){
 
-        $data=Customer::all() ;
+        // $data=Customer::paginate(5);
+
+        // return response()->json([
+        //     'success' => true,
+        //     'message' => 'Customer Data Fetched Successfully' ,
+        //     'data' => $data
+        // ],200); for simple data send  without using resource 
+        
+
         return response()->json([
-            'status' => 'Customer Data Fetched Successfully' ,
-            'data' => $data
+            'success' => true,
+            'message' => 'Customer Data Fetched Successfully' ,
+            'data' => CustomerResource::collection(Customer::paginate(5)) 
         ],200);
     }
 
     public function store(Request $request){
 
-        $request->validate([
+       $validator=validator($request->all(),[
             'name' => 'required|string|min:3' ,
             'country' => 'required|string' ,
             'gender' => 'required|string' ,
             'image' =>'required|file|max:2048' ,
             'payment' =>'required|array'
         ]);
+
+
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation Failed' ,
+                'errors' => $validator->errors()
+
+            ],422);
+        };
+
 
         $path=$request->file('image')->store('picture','public') ;
 
@@ -39,10 +60,17 @@ class CustomerController extends Controller
             'payment' => $request->payment
 
         ]);
+        // return response()->json([
+        //     'success'=>true,
+        //     'message' =>'Customer created successfully.',
+        //     'data' =>$customer_data 
+        // ] , 201);  for simple data send  without using resource 
         return response()->json([
-            'status' =>'Customer created successfully.',
-            'data' =>$customer_data 
-        ] , 201);
+            'success' => true ,
+            'message' => 'Customer Data Created' ,
+            'data' => new CustomerResource($customer_data)
 
+        ],201);  // for using resources 
+ 
     }
 }
